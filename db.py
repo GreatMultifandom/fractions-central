a498193165a475bafae13c4a6c7bb5a1117e3fb9
vezono <vezonolab@gmail.com>
2022-05-06 22:15:09 +0300

    done

--- db.py
+++ db.py
@@ -3,5 +3,15 @@ import config
 
 class Database:
     def __init__(self):
-        self.db = MongoClient(config.mongo_url)
-        self.fractions = self.db.fractions
+        self.db = MongoClient(config.mongo_url).fractions
+        self.chats = self.db.chats
+    
+    def get_chats(self):
+        return self.chats.find({})
+
+    def add_chat(self, name, chat_id):
+        return self.chats.insert_one({
+            'fraction': name,
+            'id': chat_id
+        })
+        
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 import config
 from db import Database
 
 db = Database()
 app = Client('user', config.api_id, api.hash, 'a', 'a', 'a')
+
+minecraft_bot_id = 5086631591
+gaming_chat = -1001286994009
+
+@app.on_message(filters=filters.new_chat_members)
+def new_chat(m, c):
+    if m.new_chat_members[0] != app.get_me().id:
+        return
+    app.send_message(m.chat.id, 'Для инициализации чата фракции ВМФ пропишите ".addchat имя_фракции".')
+
+@app.on_message()
+def main_handler(m, c):
+    if m.from_user.id != minecraft_bot_id and m.chat.id != gaming_chat:
+        if not m.text.startswith('.addchat '):
+            return
+        db.add_chat(m.text.split(' ', 1)[1], m.chat.id)
+        return
+    for chat in db.get_chats():
+        app.send_message(chat_id=chat['id'], text=message.text)
+
 
 app.run()
