b00c74f406b8ebc3eeb83426a795e4fba4014688
vezono <vezonolab@gmail.com>
2022-05-06 22:27:56 +0300

    done

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ gaming_chat = -1001286994009
 
 @app.on_message(filters=filters.new_chat_members)
 def new_chat(c, m):
+    print(1)
     if m.new_chat_members[0] != app.get_me().id:
         return
     app.send_message(m.chat.id, 'Для инициализации чата фракции ВМФ пропишите ".addchat имя_фракции".')
 
-@app.on_message()
+@app.on_message(filters=filters.all)
 def main_handler(c, m):
+    print(2)
     if m.from_user.id != minecraft_bot_id and m.chat.id != gaming_chat:
+        print(3)
         if not m.text.startswith('.addchat '):
             return
         db.add_chat(m.text.split(' ', 1)[1], m.chat.id)
         return
+    print(4)
     for chat in db.get_chats():
         app.send_message(chat_id=chat['id'], text=message.text)
 
